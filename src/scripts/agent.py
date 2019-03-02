e28e15834135766ecf828f25464010395a9b1c9c
racsoraul <racsoraul@gmail.com>
2019-03-01 19:29:37 -0600

    Listening to OSC messages (agent position) on 0.0.0.0:7000 and publishing it to /agent/position ROS topic.

---
+++ src/scripts/agent.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Publishes `position` of agent to the `/agent/position` topic.
+
+import rospy
+from geometry_msgs.msg import PointStamped
+from osc4py3.as_eventloop import (osc_startup, osc_udp_server, osc_method,
+    osc_process, osc_terminate)
+from osc4py3 import oscmethod as osm
+
+def handleOSCMessage(x, y, z):
+    positionStr = "Position(%s, %s, %s)"%(x, y, z)
+    rospy.loginfo(positionStr)
+    position = PointStamped()
+    position.header.stamp = rospy.Time.now()
+    position.header.frame_id = "pos"
+    position.point.x = float(x)
+    position.point.y = float(y)
+    position.point.z = float(z)
+    pub.publish(position)
+    rate.sleep()
+
+osc_startup()
+osc_udp_server("0.0.0.0", 7000, "UnrealGAMS")
+
+osc_method("/agent/0/pos", handleOSCMessage)
+
+pub = rospy.Publisher('agent/position', PointStamped, queue_size=10)
+rospy.init_node('agent', anonymous=True)
+rate = rospy.Rate(10) # 10hz
+ 
+if __name__ == '__main__':
+    try:
+        while not rospy.is_shutdown():
+            osc_process()
+        osc_terminate()
+    except rospy.ROSInterruptException:
+        pass
