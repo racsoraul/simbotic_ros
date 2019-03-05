d926cc24e7d6578e61ee00c1800352c9c939b36b
racsoraul <racsoraul@gmail.com>
2019-03-05 11:04:36 -0600

    Normalized Quaternion.

--- src/scripts/agent.py
+++ src/scripts/agent.py
@@ -3,25 +3,38 @@
 # Publishes `pose` of agent to the `/agent/pose` topic.
 
 import rospy
-from geometry_msgs.msg import PoseStamped
+from math import sqrt
+from geometry_msgs.msg import PoseStamped, Quaternion
 from osc4py3.as_eventloop import (osc_startup, osc_udp_server, osc_method,
-    osc_process, osc_terminate)
+                                  osc_process, osc_terminate)
 from osc4py3 import oscmethod as osm
 
+
 def handleOSCPoseMessage(px, py, pz, qx, qy, qz, qw):
-    poseStr = "Position(%s, %s, %s), Quaternion(%s, %s, %s, %s)" % (
-        px, py, pz, qx, qy, qz, qw)
-    rospy.loginfo(poseStr)
     agentP = PoseStamped()
+
     agentP.header.stamp = rospy.Time.now()
     agentP.header.frame_id = "unreal"
     agentP.pose.position.x = float(px)/100
     agentP.pose.position.y = -float(py)/100
     agentP.pose.position.z = float(pz)/100
-    agentP.pose.orientation.x = float(qx)/100
-    agentP.pose.orientation.y = float(qy)/100
-    agentP.pose.orientation.z = -float(qz)/100
-    agentP.pose.orientation.w = float(qw)/100
+    quatX = float(qx)/100
+    quatY = float(qy)/100
+    quatZ = float(qz)/100
+    quatW = float(qw)/100    
+    quatLen = sqrt(quatX**2 + quatY**2 + quatZ**2 + quatW**2)
+
+    # Normalized quaternion
+    agentP.pose.orientation.x = quatX/quatLen
+    agentP.pose.orientation.y = quatY/quatLen
+    agentP.pose.orientation.z = -quatZ/quatLen
+    agentP.pose.orientation.w = quatW/quatLen
+
+    poseStr = "Position(%s, %s, %s), Quaternion(%s, %s, %s, %s)" % (
+        agentP.pose.position.x, agentP.pose.position.x, agentP.pose.position.x, 
+        agentP.pose.orientation.x, agentP.pose.orientation.y, agentP.pose.orientation.z, agentP.pose.orientation.w)
+    rospy.loginfo(poseStr)
+
     pub.publish(agentP)
     rate.sleep()
 
@@ -32,8 +45,8 @@ osc_method("/agent/0/pose", handleOSCPoseMessage)
 
 pub = rospy.Publisher('agent/pose', PoseStamped, queue_size=10)
 rospy.init_node('agent', anonymous=True)
-rate = rospy.Rate(10) # 10hz
- 
+rate = rospy.Rate(10)  # 10hz
+
 if __name__ == '__main__':
     try:
         while not rospy.is_shutdown():
